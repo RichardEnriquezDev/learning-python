ea9537731a9229a666fe0f9b3ac1e1de56054eb3
Richard <richard.enriquez.dev@gmail.com>
2025-12-19 15:15:51 -0300

    Add basics: data types and variables

---
+++ python/00_datos_variables.py
@@ -0,0 +1,66 @@
+"""
+Primer ejercicio en Python.
+Conceptos básicos: comentarios, variables y tipos de datos.
+"""
+
+# Estos ejercicios los hago en conjunto y supervisado por Tera AI
+
+# La página de web del lenguaje que estudio es: https://www.python.org/
+
+# Este almohadilla simple, o numeral, me permite hacer comentarios como este en una sola línea
+
+# Un docstring se usa para documentar módulos, funciones o clases. Se utilizan tres comillas """ texto """ Como se aprecia en el encabezamiento de este archivo
+
+##########################################################################
+
+# Variables y constantes
+
+my_name = "Richard"
+
+# La expreción "my_name" es la variable, el signo igual, "=", es la asignación que le daremos a la variable y "Richard" es el valor asignado a la variable
+
+# En Python no existen constantes estrictas; por convención se usan mayúsculas.
+
+NACIONALIDAD = "Argentina"
+
+########################################################################################
+
+# A continuación se ejemplifica algunos tipos de datos primitivos en Python
+
+variable_string = "Esta es una variable string" # string - variable de tipo texto
+
+variable_int = 48 # int - variable de tipo numérico
+
+variable_float = 3.14 # float - variable que representa a decimales
+
+#########################################################################################
+
+# Otro tipo de dato representa expresiones booleanas, verdadero o falso
+
+variable_bool = True
+otra_variable_bool = False
+
+# En Python, los valores booleanos True y False deben escribirse con mayúscula inicial
+
+#########################################################################################
+
+# A continuación se muestran algunas estructuras de datos en Python
+
+my_lista = ["Teclado", "Raton", "Monitor"]
+
+my_tupla = (1, 3, 5, "primos",  [my_lista])
+
+my_sets = {"Argumentos", 15, 17, False}
+
+my_diccionario = {"Nombre" : "Richard", "Edad" : 50}
+
+# Estos tipos de variables también pueden contener todo tipo de datos, texto, número, booleano, etc
+
+# Es importante respetar los signos, paréntesis, llaves, etc, para que el programa funcine correctamente
+
+### NOTA: Solo se muestran las estructuras y no sus funciones - Se analizarán más adelante
+ 
+print ("Hola Python")
+
+# print() es una función que nos muestra información por consola 
+# Esta nos permitirá ver los resultados de nuestros programas por pantalla
